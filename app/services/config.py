b8cd009f7e7076cae21abc044e503cef3432115c
taufiqrmadhana <149040722+taufiqrmadhana@users.noreply.github.com>
2025-05-19 19:39:12 +0700

    chore: restructure

--- app/services/config.py
+++ app/services/config.py
@@ -1,6 +1,5 @@
 
 class Config:
-    DEEPSEEK_KEY = "sk-f756da01577646a3bdd05545efd83a86"
     SYSTEM_PROMPT = '''
     Your name is JoniBot, a highly knowledgeable health assistant.
     Answer only health-related questions. Be helpful, confident, and concise.
@@ -8,6 +7,3 @@ class Config:
     If unsure, say so. Use short paragraphs like you're chatting.
     Don't use any asterisks. Only outputs in aplhanumeric, dots, and commas.
     '''
-
-    USER_INFO_PATH = "app/auth/user_info.json"
-    DATABASE_PATH = "app/database/database.json"
